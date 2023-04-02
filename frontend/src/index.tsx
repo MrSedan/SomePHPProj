84e5ff2c4d1fdc3db639ebb0189d2accd56b4c0d
Sergey Elpashev <elpashevs@bk.ru>
2023-04-02 12:21:23 +0300

    Initial

---
+++ frontend/src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import Home from "./views/Home";
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+root.render(<Home />);
---
+++ frontend/src/views/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="home" style={{ display: "flex", alignSelf: "center" }}>
+            Hello, World!
+        </div>
+    );
+}
