4046912cd0d9192cf49494d8b3d6b102c1708b46
Sergey Elpashev <elpashevs@bk.ru>
2023-04-15 00:27:57 +0300

    index.tsx with router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,24 @@
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./views/Home";
+import Game from "./views/Game";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "game",
+        element: <Game />,
+    },
+    {
+        path: "*",
+        element: <p>Not found</p>,
+    },
+]);
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
